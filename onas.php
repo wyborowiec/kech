4cc352adad8d58b993f76a1d5325a4e93b915842
Wacław Borowiec <wyborowiec@gmail.com>
2015-12-05 22:37:22 +0100

    o nas

---
+++ onas.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Template Name: Onas
+*/
+
+get_header(); 
+
+?>
+
+<div class="content_padding">
+<div class="onas"/>
+	<h1>O NAS</h1>
+	<h2 class="box_tag left_right_padding">Kim jesteśmy?</h2>
+	<p>Kościół Ewangelicznych Chrześcijan wywodzi się z nurtu kościołów protestanckich powstałych w XVI wieku, w okresie Wielkiej Reformacji Kościoła Katolickiego.</p>
+
+<p>Ruch Reformacyjny wyrósł na gruncie niezadowolenia z sytuacji istniejącej w ówczesnym Kościele. Duchowni i osoby świeckie w wyniku rozważania Pisma Świętego dostrzegli, że ówczesny Kościół oddalił się od nauki Jezusa Chrystusa i Apostołów. Intencją reformatorów było doświadczenie duchowego przebudzenia i dokonanie zwrotu od ludzkiej tradycji do nauki Pisma Świętego. Niezrozumiani i prześladowani ostatecznie zostali wyłączeni z Kościoła. W rezultacie tego powstało wiele wyznań protestanckich, z których większość przyjęła zasadę Reformacji: tylko  Łaska, tylko Pismo, tylko Wiara. Kościół Ewangelicznych Chrześcijan jest jednym z takich wyznań.</p>
+
+<p>W XIX wieku wskutek przebudzenia duchowego ruch ewangelicznych chrześcijan rozwija się na terenie Rosji, Ukrainy, a później od 1908 roku także na ziemiach Polski.</p>
+
+<p>Kościół Ewangelicznych Chrześcijan działając legalnie na mocy Ustawy o związkach wyznaniowych jest obecnie rozwijającą się mniejszość wyznaniową w naszym kraju.</p>
+
+<p>Więcej o historii Kościoła Ewangelicznych Chrześcijan przeczytasz na stronie:
+<a href="www.kech.pl/historia.php">www.kech.pl/historia.php</a></p>
+	<h2 class="box_tag left_right_padding">W co wierzymy?</h2>
+	<p><ol>
+
+    <li>Nieomylność i wystarczalność Pisma Świętego jako Słowa Bożego stanowiącego jedyny, natchniony przez Boga autorytet w sprawach wiary i życia.</li>
+    <li>Jedynego Boga odwiecznie istniejącego w trzech osobach: Ojca, Syna i Ducha Świętego.</li>
+    <li>Jezusa Chrystusa, objawionego w ciele, poczętego z Ducha Świętego, narodzonego z Marii dziewicy; Jego bezgrzeszne życie i odkupieńczą śmierć na krzyżu za grzechy Świata,
+    Jego zmartwychwstanie, wniebowstąpienie i powtórne przyjście.</li>
+    <li>Zbawienie grzesznego człowieka przez wiarę w odkupieńczą śmierć Jezusa Chrystusa, z łaski – niezależnie od uczynków.</li>
+    <li>Konieczność nawrócenia się, pokuty za grzechy, odrodzenia przez Słowo Boże i Ducha Świętego, chrztu wiary przez zanurzenie, oraz uznania, że miłość Boga i bliźniego jest podstawą życia chrześcijańskiego.</li>
+    <li>Ducha Świętego zamieszkującego w wierzących, uzdalniającego do świętego życia i służby.</li>
+    <li>Powszechne kapłaństwo zbawionych.</li>
+    <li>Aktualność Wieczerzy Pańskiej pod dwoma postaciami chleba i wina jako pamiątki ofiary Pana Jezusa Chrystusa.</li>
+    <li>Powszechne zmartwychwstanie i życie wieczne jako dar Bożego miłosierdzia oraz wieczne potępienie jako wyraz Bożej sprawiedliwości.</li>
+</ol>
+</p>
+	<h2 class="box_tag left_right_padding">Jakie mamy cele?</h2>
+	
+	<p>
+	Celem Kościoła Ewangelicznych Chrześcijan jest w szczególności:</p>
+<ul>
+    <li>oddawanie Bogu chwały</li>
+    <li>głoszenie objawienia Bożego danego w Piśmie Świętym Starego i Nowego Testamentu</li>
+    <li>krzewienie Ewangelii pośród wszystkich ludzi i czynienie ich uczniami Chrystusa</li>
+    <li>głoszenie zasad etyki chrześcijańskiej i wartości wynikających z lektury Pisma Świętego</li>
+    <li>chrześcijańskie wychowanie dzieci i młodzieży</li>
+    <li>opieka duszpasterska nad katechumenami, sympatykami i ich dziećmi</li>
+    <li>działalność misyjna</li>
+    <li>działalność oświatowo-wychowawcza</li>
+    <li>działalność charytatywna</li>
+    <li>działalność opiekuńcza i społeczna</li>
+    <li>działalność resocjalizacyjna i profilaktyczna</li>
+	</ul>
+<p>Szczegółowo cele Kościoła określa Statut Kościoła Ewangelicznych Chrześcijan w RP.
+	</p>
+	</div>
+</div>
+
+<?php
+
+get_footer();
+
+?>
