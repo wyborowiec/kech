4fa66eb4cc428902c6105218fc924f1f5795fc8b
Wacław Borowiec <wyborowiec@gmail.com>
2016-01-31 22:48:20 +0100

    gallery styling errors

--- category-galeria.php
+++ category-galeria.php
@@ -44,13 +44,13 @@ get_header(); ?>
 						$thumb_file = wp_get_attachment_image($first_img_id, 'gallery-thumbnail');
 						$author = get_post_meta($id, "Autor", true);
 						$title = get_the_title();
-						$gallery_icon_class = $i % 4 == 0 ? "" : "gallery_icon";
+						$gallery_icon_class = (($i % 4) == 0) ? "gallery_icon2" : "gallery_icon";
 						$gallery_margin_bottom_class = $i <= 4 ? "gallery_margin_bottom" : "";
 						$date = get_the_date("j.m.Y");
 						$link = wp_get_shortlink();
 						?>
 						<a href="<?php echo $link; ?>">
-						<div class="<?php echo "$gallery_icon_class $gallery_margin_bottom_class"; ?>">
+						<div class="frame <?php echo "$gallery_icon_class $gallery_margin_bottom_class"; ?>">
 							<div class="gallery_thumb">
 								<?php echo $thumb_file; ?>
 							</div>
@@ -61,7 +61,11 @@ get_header(); ?>
 								<?php echo $date; ?>
 							</div>
 						</div>
+						</a>
 				<?php
+						if (($i % 4) == 0) {
+							echo "<div class=\"gallery_clear\"></div>";
+						}
 						$i++;
 					endwhile;
 					wp_reset_postdata();
