a33cec43a30a2c3fd477b394c99378bc6aea77b2
Wacław Borowiec <waclaw.borowiec@gridx.com>
2021-05-03 21:16:01 +0200

    recaptcha filter dla wysyłania emaili

--- header.php
+++ header.php
@@ -5,18 +5,20 @@
 <meta charset="utf-8">
 
 <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css">
-<?php wp_head(); ?>
+<?php
+	wp_head();
+	include 'config.php';
+?>
 </head>
 <body background="<?php echo get_template_directory_uri(); ?>/pics/tlo.gif">
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/datetimepicker/jquery.datetimepicker.css"/ >
 <div id="container">
 <a href="<?php echo home_url();?>"><img src="<?php bloginfo('template_directory'); ?>/pics/banner.jpg" width="1024"/></a>
-<div id="calendar_div"><a id="calendar" href="calendar-2"><img src="<?php bloginfo('template_directory'); ?>/pics/kalendarz.png""/></a></div>
+<div id="calendar_div"><a id="calendar" href="/wordpress/kalendarz"><img src="<?php bloginfo('template_directory'); ?>/pics/kalendarz.png""/></a></div>
 
 <div id="header">
 
-<?php 
-	wp_nav_menu(array('theme_location' => 'header-menu', 'container_class' => 'main-menu')); 
+<?php
+	wp_nav_menu(array('theme_location' => 'header-menu', 'container_class' => 'main-menu'));
 ?>
 </div>
-
--- kontakt.php
+++ kontakt.php
@@ -3,20 +3,41 @@
 Template Name: Kontakt
 */
 
-get_header(); 
-//print_r($_POST);
-//the_permalink(); 
+get_header();
+define("RECAPTCHA_V3_ACTION", 'send_email');
 $send = isset($_POST['kech_msg']);
+$message = "";
 if ($send) {
-	send_mail();
+	$token = $_POST['token'];
+	$action = $_POST['action'];
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	$arrResponse = json_decode($response, true);
+	//echo $response."<br>";
+
+	// verify the response
+	if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+	    send_mail();
+			$message = "<p>Wiadomość została wysłana. Dziękujemy.</p>";
+	} else {
+			$message = "<p>Wystąpił błąd, wiadomość nie została wysłana.</p>";
+	}
 }
 
 ?>
-
+<script
+      src="https://code.jquery.com/jquery-3.4.1.min.js"
+      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+      crossorigin="anonymous"></script>
+<script src="https://www.google.com/recaptcha/api.js?render=<?php echo RECAPTCHA_V3_KEY ?>"></script>
 <div class="content_padding">
-	<?php if ($send) { ?>
-			<p>Wiadomość została wysłana. Dziękujemy.</p>
-	<?php }; ?>
+	<?php echo $message; ?>
 	<div class="contact_info">
 		<h1>KONTAKT</h1>
 		<h2>Kościół Ewangelicznych Chrześcijan w Pszczynie</br>
@@ -30,7 +51,7 @@ if ($send) {
 		<p>Inspektor Danych Osobowych Kościoła Ewangelicznych Chrześcijan w RP<br/>E-mail: InspektorDanych@kech.pl</p>
 		<p><a href="http://www.pszczyna.kech.pl/wp-content/uploads/2018/05/Regulamin-ODO-w-KECh-16.04.2018.pdf">Regulamin ODO w KECh</a></p>
 		<p><a href="http://www.pszczyna.kech.pl/wp-content/uploads/2018/05/INSTRUKCJA-ODO-w-KECh.pdf">Instrukcja ODO w KECh</a></p>
-		<iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=pszczyna%20dworcowa%2020&key=AIzaSyDUtO9fwZ270mCi8FSJlU62k4HRbxCb8GM"></iframe> 
+		<iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=pszczyna%20dworcowa%2020&key=AIzaSyDUtO9fwZ270mCi8FSJlU62k4HRbxCb8GM"></iframe>
 	</div>
 	<div class="contact_form">
 		<h1>FORMULARZ KONTAKTOWY</h1>
@@ -38,8 +59,8 @@ if ($send) {
 			<?php
 				if (false) { ?>
 					<p>Dziękujemy, wiadomość została wysłana.</p>
-				<?php 
-				} else { 
+				<?php
+				} else {
 				?>
 					<form action="<?php the_permalink(); ?>" method="post" id="contactForm">
 						<span class="contact_form_content_label">Imię i nazwisko:</span>
@@ -56,6 +77,18 @@ if ($send) {
 						<input type="checkbox" name="kech_to_self" id="kech_to_self"/>
 						<input class="contact_form_content_button" type="submit" value="Wyślij" ></input>
 					</form>
+					<script>
+			    	$('#contactForm').submit(function(event) {
+			        event.preventDefault();
+			        grecaptcha.ready(function() {
+			            grecaptcha.execute('<?php echo RECAPTCHA_V3_KEY ?>', {action: '<?php echo RECAPTCHA_V3_ACTION ?>'}).then(function(token) {
+			                $('#contactForm').prepend('<input type="hidden" name="token" value="' + token + '">');
+			                $('#contactForm').prepend('<input type="hidden" name="action" value="<?php echo RECAPTCHA_V3_ACTION ?>">');
+			                $('#contactForm').unbind('submit').submit();
+			            });;
+			        });
+			  		});
+  				</script>
 			<?php
 				}
 			?>
